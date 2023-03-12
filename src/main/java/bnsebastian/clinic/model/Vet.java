d39b1f521c26f812dd9d2d19d17a285a94f105b7
Sebastian BN <105242756+BNSebastian@users.noreply.github.com>
2023-03-12 18:25:05 +0200

    update

--- src/main/java/bnsebastian/clinic/model/Vet.java
+++ src/main/java/bnsebastian/clinic/model/Vet.java
@@ -1,24 +1,22 @@
 package bnsebastian.clinic.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person{
 
     @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
-                                        inverseJoinColumns = @JoinColumn(name = "specialty_id"))
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Speciality> specialties = new HashSet<>();
 
-    public Set<Speciality> getSpecialties() {
-        return specialties;
-    }
-
-    public void setSpecialties(Set<Speciality> specialties) {
-        this.specialties = specialties;
-    }
 }
